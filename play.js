fb0b9eec0417f2facc6212316afba5ed41d975a2
Fransiskus <aldzaidea@gmail.com>
2022-01-19 19:41:46 +0000

    create clients js and export to play.js

--- play.js
+++ play.js
@@ -1,18 +1,3 @@
-const net = require("net");
+const { connect } = require("./client");
 
-const connect = () => {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
